83344024e99f4cc124d1fd9c687959639db642c6
Dimmies <dimmiestv@gmail.com>
2020-01-02 16:07:46 -0500

    Update sv_main.lua

--- savecoords/sv_main.lua
+++ savecoords/sv_main.lua
@@ -5,14 +5,13 @@ end)
 
 function SaveCoords(player, name, x, y, z)
     plyName = GetPlayerName(player)
-    file = io.open(plyName.."-Coords.txt", "a")
+    file = io.open(plyName.."-SavedCoords.txt", "a")
     if name == nil then
         AddPlayerChat(player, "Proper Usage : /pos <name>")
     end
     if file then
         file:write(name .." | " ..x.. ", " ..y.. ", " ..z.. "\n")
-        file:write(name ..' | {'.. '["x"] = ' ..x.. ', ["y"] = ' ..y.. ', ["z"] = ' ..z.. '},\n')
-        AddPlayerChat(player, "Saved Position " ..name.. " X: " ..x.. " Y: " ..y.. " Z: " ..z.. " to ".. plyName.."-Coords.txt")
+        AddPlayerChat(player, "Saved Position " ..name.. " X: " ..x.. " Y: " ..y.. " Z: " ..z.. " to ".. plyName.."-SavedCoords.txt")
     end
     file:close()
 end
