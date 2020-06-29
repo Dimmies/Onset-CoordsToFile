63a4bd45f7527587b66fd4e64ea20d050fc51aea
Roman Wanner <roman_wanner@rowa-digital.ch>
2020-06-29 12:09:09 -0300

    Update sv_main.lua
    Added vehicle check and added height to the position

--- savecoords/sv_main.lua
+++ savecoords/sv_main.lua
@@ -1,17 +1,27 @@
 AddCommand("pos", function(player, name)
     x, y, z = GetPlayerLocation(player)
-    SaveCoords(player, name, x, y, z)
+    h = GetPlayerHeading(player)
+    SaveCoords(player, name, x, y, z, h)
 end)
 
-function SaveCoords(player, name, x, y, z)
+function SaveCoords(player, name, x, y, z, h)
     plyName = GetPlayerName(player)
     file = io.open(plyName.."-SavedCoords.txt", "a")
     if name == nil then
         AddPlayerChat(player, "Proper Usage : /pos <name>")
     end
+    
     if file then
-        file:write(name .." | " ..x.. ", " ..y.. ", " ..z.. "\n")
-        AddPlayerChat(player, "Saved Position " ..name.. " X: " ..x.. " Y: " ..y.. " Z: " ..z.. " to ".. plyName.."-SavedCoords.txt")
+        local pvehicle = GetPlayerVehicle(player)
+        if(pvehicle == 0) then
+            file:write(name .." | " ..x.. ", " ..y.. ", " ..z.. ":" ..h.."\n")
+            AddPlayerChat(player, "Saved Position " ..name.. " X: " ..x.. " Y: " ..y.. " Z: " ..z.. "H: " ..h.. " to ".. plyName.."-SavedCoords.txt")
+        else
+            local x,y,z = GetVehicleLocation(pvehicle)
+            local h = GetVehicleHeading(pvehicle)
+            file:write("vehicle-"..name.." | "..x..", "..y..", "..z..":"..h.."\n")
+            AddPlayerChat(player, "Saved Vehicleposition "..name.." X: "..x.." Y: "..y.." Z: "..z.." H: "..h.." to "..plyName.."-SavedCoords.txt")
+        end
     end
     file:close()
 end
